1df4dede812cc7a5ed807f35113d52078bea099c
E3BB <78990896+E3BB@users.noreply.github.com>
2024-05-19 14:57:06 +0000

    Display finished; now need to make wipe_display() wipe the display.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # display-py-demo
-This is python display demo. No modules are needed; just print(), for loops and (nested) arrays
+This is python display demo. No modules are needed; just print(), for loops and nested arrays
+
+## Function descriptions:
+### wipe_display()
+Initializes (or wipes) display. ***Always use this before other display functions!***
+
+This function resets the current display, then refills it with the current 
+blank_letter.
+
+To change the display size, **change dispx and dispy before initializing
+the display.**
+
+### print_display()
+Prints current display to terminal and adds a blank line.
+
+Example:
+```
+L L L L
+L L L L
+L L L L
+
+```
+*Takes no values!*
+
+### change_pixel(x,y,char)
+Changes character in display at position of (x,y).
+
+**Keep in mind, x/y values go from 0 to (display_size - 1)**;
+*also char is string of length 1.*
+
+Also, y=0 is top of display, y=dispy is bottom.
+```
+change_pixel(3,2,"j")
+print_display()
+
+I I I I
+I I I I
+I I I j
+I I I I
+```
+
+## Variables
+
---
+++ main.py
@@ -0,0 +1,44 @@
+# Display module - Elliott
+
+display = [] # To access given cell after filling, do display[y][x]
+dispx = 16
+dispy = 9
+blank_letter = "I"
+
+def wipe_display():
+    #display = []
+
+    for i in range(dispy):
+        new_arr = []
+        for o in range(dispx):
+            new_arr.append(blank_letter + " ") # + " " b/c norm disp = NNNN, but w/ spc = N N N N
+        display.append([x for x in new_arr]) # Appends new_arr to (end of) disp.x
+
+def print_display():
+    for y_buffer in display:
+        new_line = ""
+        for x in y_buffer:
+            new_line = new_line + x
+        print(new_line)
+    print()
+
+def change_pixel(x:int, y:int, char:str):
+    if x<0: x=0 # X-value checks
+    elif x>(dispx-1): x=dispx-1
+    
+    if y<0: y=0 # Y-value checks
+    elif y>(dispy-1): y=dispy-1
+
+    if len(char) > 1: print("Char too long; new char is ", char[0])
+    char = char[0] # Sets character to only first value
+    
+    display[y][x] = char + " " # Sets cell to character
+
+wipe_display()
+print_display()
+change_pixel(3,2,"A")
+print_display()
+
+wipe_display()
+
+print_display()
